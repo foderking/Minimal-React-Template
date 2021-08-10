b8e8e558f2116369c387f2eaf527fdce8bbb0929
foderking <freatshow@gmail.com>
2021-08-10 01:26:15 -0700

    change from npm to yarn

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import { Button } from 'react-bootstrap'
+// import { Button } from 'react-bootstrap'
 
 const App = () =>
 {
   return (
     <div className='container'>
       <h1>Hello World!</h1>
-      <Button onClick={() => toggleShow(true)}>Show Toast</Button>
     </div>
   )
 }
